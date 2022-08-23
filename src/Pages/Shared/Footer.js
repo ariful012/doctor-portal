8a18f4f997087a444ada8af69d034cb278e2229a
ariful012 <ariful.diu.cse@gmail.com>
2022-08-23 07:14:22 +0600

    footer add

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,39 +1,71 @@
-import React from 'react';
-import footer from '../../assets/images/footer.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import footer from "../../assets/images/footer.png";
 
 const Footer = () => {
-    return (
-        <footer style={{
-            background: `url(${footer})`,
-            backgroundSize: 'cover'
-        }} className="p-10">
-            <div className='footer'>
-                <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </div>
-                <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </div>
-            </div>
-            <div className='my-10 text-center'>
-                <p>Copyright © 2022 - All right reserved</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer
+      style={{
+        background: `url(${footer})`,
+        backgroundSize: "cover",
+      }}
+      className="p-10 mt-10"
+    >
+      <div className="footer">
+        <div>
+          <span className="footer-title text-2xl">Contact</span>
+          <div className="text-xl">
+            <p>Address:</p>
+            <p>Mirpur 10, Dhaka 1216</p>
+            <p>Email:</p>
+            <p>ariful25-013@diu.edu.bd</p>
+            <p>Phone:</p>
+            <p>+8801832549198</p>
+            <p>Opening Hours:</p>
+            <p>10:00 AM to 08:00 PM</p>
+          </div>
+        </div>
+        <div>
+          <div className="text-xl list-none uppercase">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/appointment">Appointment</Link>
+            </li>
+            <li>
+              <Link to="/doctor">Doctor</Link>
+            </li>
+            <li>
+              <Link to="/review">Review</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </div>
+        </div>
+        <div>
+          <div className="text-xl list-none uppercase">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">sign up</Link>
+            </li>
+            <li>
+              <Link to="/signup">registration</Link>
+            </li>
+          </div>
+        </div>
+      </div>
+      <div className="my-10 text-center">
+        <p>Copyright © 2022 - All right reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
