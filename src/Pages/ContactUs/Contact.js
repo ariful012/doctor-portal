b953379fed2bf846dcc6ff46463b89892c6c08a1
ariful012 <ariful.diu.cse@gmail.com>
2022-08-23 16:18:15 +0600

    contact page add

--- src/Pages/ContactUs/Contact.js
+++ src/Pages/ContactUs/Contact.js
@@ -1,8 +1,34 @@
-import React from "react";
+import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
+import { toast } from "react-toastify";
 import appointment from "../../assets/images/appointment.png";
 import PrimaryButton from "../Shared/PrimaryButton";
-
 const Contact = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_z31ujqj",
+        "template_zbiqelp",
+        form.current,
+        "w15iQbZy-fBTknB8a"
+      )
+      .then(
+        (result) => {
+          toast.success(
+            "Thanks for your report we will reply it in very soon..."
+          );
+          console.log(form);
+        },
+        (error) => {
+          toast.error("Message Not Send...");
+        }
+      );
+  };
+
   return (
     <div
       style={{
@@ -16,24 +42,81 @@ const Contact = () => {
         </p>
         <h1 className="text-4xl">Stay connected with us</h1>
       </div>
-      <div className="grid grid-cols-1 justify-items-center gap-5">
+      <form
+        style={{
+          width: "400px",
+          margin: "auto",
+          padding: "20px 10px",
+        }}
+        ref={form}
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
-          placeholder="Email Address"
-          className="input w-full max-w-md"
+          placeholder="Write your Name ..."
+          required
+          className="p-2"
+          style={{
+            border: "none",
+            outline: "none",
+            width: "100%",
+            borderBottom: "1px solid #3BB77E",
+            margin: "10px 0",
+            fontSize: "1.2vmax",
+            height: "40px",
+          }}
+          name="name"
         />
         <input
+          className="p-2"
           type="text"
-          placeholder="Subject"
-          className="input w-full max-w-md"
+          placeholder="Write a Subject ..."
+          required
+          style={{
+            border: "none",
+            outline: "none",
+            width: "100%",
+            borderBottom: "1px solid #3BB77E",
+            margin: "10px 0",
+            fontSize: "1.2vmax",
+            height: "40px",
+          }}
+          name="subject"
+        />
+        <input
+          type="email"
+          className="p-2"
+          placeholder="write your Email ..."
+          required
+          style={{
+            border: "none",
+            outline: "none",
+            width: "100%",
+            borderBottom: "1px solid #3BB77E",
+            margin: "10px 0",
+            fontSize: "1.2vmax",
+            height: "40px",
+          }}
+          name="email"
         />
         <textarea
-          className="textarea w-full max-w-md"
-          placeholder="Your message"
-          rows={6}
+          cols="30"
+          rows="5"
+          required
+          className="p-2"
+          placeholder="write your message ..."
+          style={{
+            border: "none",
+            outline: "none",
+            width: "100%",
+            borderBottom: "1px solid #3BB77E",
+            margin: "10px 0",
+            fontSize: "1.2vmax",
+          }}
+          name="message"
         ></textarea>
         <PrimaryButton>Submit</PrimaryButton>
-      </div>
+      </form>
     </div>
   );
 };
