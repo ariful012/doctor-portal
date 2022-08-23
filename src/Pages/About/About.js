ec63534972c6e0aab44756dd8283046ba3171cb6
ariful012 <ariful.diu.cse@gmail.com>
2022-08-23 16:50:55 +0600

    about page added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import appointment from "../../assets/images/appointment.png";
 
 const About = () => {
-    return (
-        <div>
-            <h2>This is About</h2>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        background: `url(${appointment})`,
+      }}
+      className="bg-primary px-10 py-14 mt-4 "
+    >
+      <div className="text-center pb-14 text-white">
+        <p className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary">
+          ABOUT
+        </p>
+        <p className="text-justify text-2xl p-5 mt-5">
+          The Doctors Portal provides health organizations that strive for
+          excellence and quality care, with a tool that allows physicians to
+          measure themselves against key performance indicators set by the
+          organization. This tool helps optimize physician practices in
+          compliance with the organizations guidelines, offering valuable
+          statistics. Health organizations can easily get creative with this
+          tool by setting targets for practices, and even promoting healthy
+          competition between physicians to increase engagement in providing
+          useful metrics, and overall better quality of service.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/ContactUs/Contact.js
+++ src/Pages/ContactUs/Contact.js
@@ -34,10 +34,10 @@ const Contact = () => {
       style={{
         background: `url(${appointment})`,
       }}
-      className="bg-primary px-10 py-14 "
+      className="bg-primary px-10 py-4 "
     >
-      <div className="text-center pb-14 text-white">
-        <p className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary">
+      <div className="text-center pb-5 text-white">
+        <p className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary">
           Contact Us
         </p>
         <h1 className="text-4xl">Stay connected with us</h1>
