134ae21471368042467e975139c3e685f30442b1
ariful012 <ariful.diu.cse@gmail.com>
2022-08-23 06:44:36 +0600

    doctor route added

--- src/Pages/Doctor/Doctor.js
+++ src/Pages/Doctor/Doctor.js
@@ -1,24 +1,14 @@
 import React from "react";
 
-const Doctor = ({ doctor }) => {
+const Doctor = ({ doctor, refetch }) => {
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-      <div className="card-body">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
-          voluptates quo incidunt harum minus mollitia!
-        </p>
-        <div className="flex items-center">
-          <div className="avatar">
-            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
-              <img src={doctor.img} alt="" />
-            </div>
-          </div>
-          <div>
-            <h4 className="text-xl">{doctor.name}</h4>
-            <p>{doctor.location}</p>
-          </div>
-        </div>
+      <figure className="px-10 pt-10">
+        <img src={doctor.img} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{doctor.name}</h2>
+        <p className="text-xl">{doctor.specialty}</p>
       </div>
     </div>
   );
--- src/Pages/Doctor/Doctors.js
+++ src/Pages/Doctor/Doctors.js
@@ -1,47 +1,32 @@
 import React from "react";
 import quote from "../../assets/icons/quote.svg";
-import people1 from "../../assets/images/people1.png";
-import people2 from "../../assets/images/people2.png";
-import people3 from "../../assets/images/people3.png";
+import { useQuery } from "react-query";
 import Doctor from "./Doctor";
+import Loading from "../Shared/Loading";
 const Doctors = () => {
-  const DoctorsProfile = [
-    {
-      _id: 1,
-      name: "Winson Herry",
-      review: "",
-      location: "california",
-      img: people1,
-    },
-    {
-      _id: 2,
-      name: "Winson Herry",
-      review: "",
-      location: "california",
-      img: people2,
-    },
-    {
-      _id: 3,
-      name: "Winson Herry",
-      review: "",
-      location: "california",
-      img: people3,
-    },
-  ];
+  const {
+    data: doctorsData,
+    isLoading,
+    refetch,
+  } = useQuery("doctors", () =>
+    fetch("http://localhost:5000/doctor", {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <section className="my-20">
       <h2 className="text-primary  text-3xl font-bold uppercase text-center ">
         Ours Doctors
       </h2>
-      <div className="flex justify-between">
-        <div></div>
-        <div>
-          <img src={quote} className="w-24 lg:w-48" alt="" />
-        </div>
-      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {DoctorsProfile.map((doctor) => (
-          <Doctor key={doctor._id} doctor={doctor}></Doctor>
+        {doctorsData.map((doctor) => (
+          <Doctor key={doctor._id} doctor={doctor} refetch={refetch}></Doctor>
         ))}
       </div>
     </section>
